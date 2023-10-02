dc1937b4ab5ad8993baa98c40495a8a467e65abe
Tong Xia <gregxia0625@gmail.com>
2023-10-01 23:40:24 -0500

    Implemented CRUD for Comment API

--- src/main/java/com/txia/personalBlog/controller/CommentController.java
+++ src/main/java/com/txia/personalBlog/controller/CommentController.java
@@ -36,4 +36,22 @@ public class CommentController {
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
 
     }
+
+
+    @PutMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") Long postId,
+                                                     @PathVariable(value = "id") Long commentId,
+                                                     @RequestBody  CommentDto commentDto){
+        CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable(value = "postId") Long postId,
+                                                     @PathVariable(value = "id") Long commentId){
+        commentService.deleteComment(postId, commentId);
+        return new ResponseEntity<>("Comment Successfully Deleted", HttpStatus.OK);
+
+    }
 }
--- src/main/java/com/txia/personalBlog/service/CommentService.java
+++ src/main/java/com/txia/personalBlog/service/CommentService.java
@@ -9,6 +9,8 @@ public interface CommentService {
     List<CommentDto> getCommentsByPostId(long postId);
 
     CommentDto getCommentById(long postId, long commentId);
+    CommentDto updateComment(long postId, long commentId, CommentDto commentRequest);
+    public void deleteComment(long postId, long commentId);
 
 
 }
